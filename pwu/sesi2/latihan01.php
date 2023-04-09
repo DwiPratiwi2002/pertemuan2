a41182c7d76619169d6fbbaa12b5b920d5178eba
DwiPratiwi2002 <128199835+DwiPratiwi2002@users.noreply.github.com>
2023-04-09 11:21:52 +0800

    Add files via upload

---
+++ pwu/sesi2/latihan01.php
@@ -0,0 +1,7 @@
+<?php
+    echo "Belajar Script PHP";
+    $dta = "Dwi Pratiwi";
+    $angka = 5;
+    echo '<br>Nama '.$dta." Peringkat Ke-".$angka ;
+    echo '<br>Nama $dta'. "Anak Ke-". $angka;
+    echo "<br>Nama: $dta Anak Ke-$angka";
---
+++ pwu/sesi2/latihan03.php
@@ -0,0 +1,8 @@
+<?php
+    $kondisiAwal = 7;
+
+    if($kondisiAwal == 1){
+        echo "Kondisi Awal Sama Dengan 1";
+    }else{
+        echo "Kondisi Awal Tydack Sama Dengan 1 hahahah";
+    }
